ac86c419db4c1f644e4a5e3de3f04cdc268d8f0d
NSCabezon <nscabezon@gmail.com>
2016-12-29 13:04:45 +0100

    added support for carthage

---
+++ FloatLabelExample/FloatLabelFields/FloatLabelFields.h
@@ -0,0 +1,19 @@
+//
+//  FloatLabelFields.h
+//  FloatLabelFields
+//
+//  Created by Ivan Cabezon on 29/12/16.
+//  Copyright © 2016 RookSoft Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatLabelFields.
+FOUNDATION_EXPORT double FloatLabelFieldsVersionNumber;
+
+//! Project version string for FloatLabelFields.
+FOUNDATION_EXPORT const unsigned char FloatLabelFieldsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatLabelFields/PublicHeader.h>
+
+
